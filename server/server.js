93e4b54a7bf1a3d50407ffe7ebbbeafcd2bc609d
Mayank Arya <mayanka@valuefy.com>
2019-05-05 15:08:41 +0530

    handled static files

--- server/server.js
+++ server/server.js
@@ -3,9 +3,8 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:4200'
-}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../fetchgitissues/')));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server started on port 3000');

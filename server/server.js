047463764ff87fc80a1ec45c8857e90dbaa01f3e
Mayank Arya <mayanka@valuefy.com>
2019-05-05 16:54:38 +0530

    reverted changes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+// assigning variable to this method
 const app = express();
 
+// using this middleware to handle cross origin errors
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/dist/fetchgitissues/')));
+// handling the static files which are created when dist directory is created that is on build.
+// __dirname is equivalent to current directory 
+app.use(express.static(path.join(__dirname + '../client/dist/fetchgitissues/')));
 
+// redirecting all the requests to our index.html file
 app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '../client/dist/fetchgitissues/', 'index.html'));
+    response.sendFile(path.join(__dirname + '../client/dist/fetchgitissues/' + 'index.html'));
 });
 
+// starting server on either the port specified by environment or port 3000
 app.listen(process.env.PORT || 3000, () => {
     console.log('server started...');
 })

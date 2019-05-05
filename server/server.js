dd2d9d61db789d23c6865ae04b1540bdfb576ea5
Mayank Arya <mayanka@valuefy.com>
2019-05-05 15:24:52 +0530

    added complete client

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../fetchgitissues/')));
+app.use(express.static(path.join(__dirname, '../client/dist/fetchgitissues/')));
 
 app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '../fetchgitissues/', 'index.html'));
+    response.sendFile(path.join(__dirname, '../client/dist/fetchgitissues/', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('server started on port 3000');
+    console.log('server started...');
 })
 

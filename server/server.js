f9607d77f8f6972f2a70e73411fbf9c04b98bc26
Mayank Arya <mayanka@valuefy.com>
2019-05-05 15:10:43 +0530

    handling get request

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../fetchgitissues/')));
 
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, '../fetchgitissues/', 'index.html'));
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('server started on port 3000');
 })
+
